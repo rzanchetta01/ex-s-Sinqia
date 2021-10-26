79fc4d21969e6b68144c1607f1e88a9d4e8786ff
rzanchetta01 <rzanchetta01@gmail.com>
2021-10-26 15:24:10 -0300

    Initial Api

---
+++ ApiAutomovel/Data/AppDbContext.cs
@@ -0,0 +1,49 @@
+﻿using ApiAutomovel.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAutomovel.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Automovel> Automeveis { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            AutomoveisCreation(modelBuilder);
+        }
+
+        private void AutomoveisCreation(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Automovel>()
+                        .Property(p => p.Modelo)
+                        .HasMaxLength(150);
+
+            modelBuilder.Entity<Automovel>()
+                        .Property(p => p.Placa)
+                        .HasMaxLength(8);
+
+            modelBuilder.Entity<Automovel>()
+                        .Property(p => p.Descricao)
+                        .HasMaxLength(600);
+
+            modelBuilder.Entity<Automovel>()
+                        .Property(p => p.Modelo)
+                        .HasPrecision(12, 2);
+
+            modelBuilder.Entity<Automovel>()
+                        .HasData(
+                                new Automovel { Id = 1, Descricao = "é um carro quadrado com 3 rodas", Modelo = "Ford triciclo", Placa = "asn 1111", Preco = 500.00M }
+                        );
+        }
+    }
+}
---
+++ ApiAutomovel/Migrations/20211026180219_test.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiAutomovel.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Automeveis",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Modelo = table.Column<string>(type: "nvarchar(150)", maxLength: 150, precision: 12, scale: 2, nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(600)", maxLength: 600, nullable: true),
+                    Placa = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: true),
+                    Preco = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Automeveis", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Automeveis",
+                columns: new[] { "Id", "Descricao", "Modelo", "Placa", "Preco" },
+                values: new object[] { 1, "é um carro quadrado com 3 rodas", "Ford triciclo", "asn 1111", 500.00m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Automeveis");
+        }
+    }
+}
